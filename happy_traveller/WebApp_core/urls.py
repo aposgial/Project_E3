1e3a293a70c8017431000c696a062ebfecb3a7e7
MichalisPapa19 <michpapaioannou93@gmail.com>
2022-11-02 00:56:50 +0200

    ok

--- happy_traveller/WebApp_core/urls.py
+++ happy_traveller/WebApp_core/urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.index, name='index'),
-    path
-=======
-    path('', views.home, name='home')
->>>>>>> 51263b0c059ac9f1e0a87b4e0077ad92a524b051
+    path('', views.home, name='home'),
 ]

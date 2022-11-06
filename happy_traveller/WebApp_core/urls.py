801836f4cb2cfe256c5d020d315cb8dbb1f15de2
mariosp09 <mariosp09@hotmail.com>
2022-11-06 21:03:31 +0200

    general htmal cheanges

--- happy_traveller/WebApp_core/urls.py
+++ happy_traveller/WebApp_core/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('map', views.map, name='map')
+    path('map', views.map, name='map'),
+    path('maptest', views.maptest, name='maptest'),
+
+    path('im', views.im, name='im'),
 ]

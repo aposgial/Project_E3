71ade048c66ce5ce75a4f2cd24405fb019f67306
mariosp09 <mariosp09@hotmail.com>
2022-11-10 00:16:46 +0200

    stisimo_homepage

--- happy_traveller/WebApp_core/urls.py
+++ happy_traveller/WebApp_core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('/templates/register/sign_in', views.login, name='login')
 ]
--- happy_traveller/WebApp_core/views.py
+++ happy_traveller/WebApp_core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'WebApp_core/home.html')
+
+def login(request):
+    return render(request, 'WebApp_core/sign_in.html')

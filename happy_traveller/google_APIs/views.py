0ae2cf81196b7d0822afb3e414d532657b0e8aa4
MichalisPapa19 <michpapaioannou93@gmail.com>
2022-11-11 01:45:47 +0200

    direction and address

--- happy_traveller/google_APIs/views.py
+++ happy_traveller/google_APIs/views.py
@@ -24,9 +24,10 @@ def api_data(request):
                                         departure_time=now)
     
     data['geocode_rs'] = directions_result
-    
 
-    result = gmaps.find_place(input=['Athens'], input_type='textquery')['candidates'][0]
+    location_name = 'ΜΑΝΤΑΜ ΜΕΖΕΔΟΠΩΛΕΙΟ' 
+
+    result = gmaps.find_place(input= location_name, input_type='textquery')['candidates'][0]
 
     photos:list = gmaps.place(place_id=result['place_id'], fields=['photo'])['result']['photos']
    
@@ -39,17 +40,21 @@ def api_data(request):
     f.close()
 
 
-    places_name = gmaps.place(place_id=result['place_id'],fields=['name'])['result']
+    places_name = gmaps.place(place_id=result['place_id'],fields=['name'])['result']['name']
     data['places_nm'] = places_name
 
     
-    location_name = 'Athens'
+    places_Address = gmaps.places(query = location_name )['results'][0]['formatted_address']
+    data['places_adr'] = places_Address
+
+    places_location_lat = gmaps.places(query = location_name )['results'][0]['geometry']['location']['lat']
+    data['places_loclat'] = places_location_lat
+    places_location_lng = gmaps.places(query = location_name )['results'][0]['geometry']['location']['lng']
+    data['places_loclng'] = places_location_lng
+
     places_info = gmaps.places(query = location_name )
     data['places_inf'] = places_info
-
-    find_place_rs = gmaps.find_place(input=['Athens'], input_type='textquery')
-    data['places_fp'] = find_place_rs
-
+   
     
  
  

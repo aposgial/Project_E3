a608654dbd97bc8a12f8de4b1d1662c374abedf2
aposgial <apostolosgls@gmail.com>
2022-11-14 12:32:23 +0200

    google maps api controller bug fixes

--- happy_traveller/google_APIs/controller.py
+++ happy_traveller/google_APIs/controller.py
@@ -7,38 +7,109 @@ class API_Controller():
         self.search_location = search_location
 
 
-    def _get_place_name(self) -> str:
+    def get_photo(self, photo_reference:str) -> list:
         try:
-            result = self.client.find_place(input=self.search_location, input_type='textquery')['candidates'][0]
-            return self.client.place(place_id=result['place_id'],fields=['name'])['result']['name']
-        except:
-            return ''
-
-    def get_photos(self):
-        result = self.client.find_place(input=self.search_location, input_type='textquery')['candidates'][0]
-        photos:list = self.client.place(place_id=result['place_id'], fields=['photo'])['result']['photos']
-    
-        for index, photo in enumerate(photos):
-            finall_photo = self.client.places_photo(photo_reference=photo['photo_reference'], max_width=400, max_height=400)
-            with open('static/photos/photo{}.jpg'.format(index), 'wb') as f:
+            photo_name = photo_reference + '.jpg'
+            finall_photo = self.client.places_photo(photo_reference=photo_reference, max_width=400, max_height=400)
+            
+            with open('photos/{}'.format(photo_name), 'wb') as f:
                 for chunk in finall_photo:
                     if chunk:
                         f.write(chunk)
+            return photo_name
+        except:
+            return ''
+
+    def get_photos(self, place_id:str):
+        photos_names = []
+        try:
+            photos:list = self.client.place(place_id=place_id, fields=['photo'])['result']['photos']
+        
+            for photo in photos:
+                photo_name = str(photo['photo_reference']) + '.jpg'
+                finall_photo = self.client.places_photo(photo_reference=photo['photo_reference'], max_width=400, max_height=400)
+
+                with open('photos/{}'.format(photo_name), 'wb') as f:
+                    for chunk in finall_photo:
+                        if chunk:
+                            photos_names.append(photo_name)
+                            f.write(chunk)
+
+            return photos_names
+        except Exception as e:
+            print(e)
+            return []
 
     
-    def get_place_info(self):
+    def get_places_info(self, option:str):
+        data = []
+        results:list = self.client.places(query=self.search_location)['results']
         try:
-            result = self.client.places(query=self.search_location )
-            print(result)
-            return {
-                "name": self._get_place_name(),
-                "open_now": result['results'][0]['opening_hours']['open_now'],
-                "address": result['results'][0]['formatted_address'],
-                "rating": result['results'][0]['rating'],
-                "loclat": result['results'][0]['geometry']['location']['lat'],
-                "loclng": result['results'][0]['geometry']['location']['lng']
-            }
-        except:
-            return {
-                "name": self.get_place_info()
-            }
+            if option == 'place':
+                for result in results:
+                    if result['name']:
+                        name = result['name']
+
+                    if 'opening_hours' in result:
+                        open_now = result['opening_hours']['open_now']
+                    else:
+                        open_now = None
+
+                    if 'formatted_address' in result:
+                        formatted_address = result['formatted_address']
+
+                    if 'rating' in result:
+                        rating = result['rating']
+                    else:
+                        rating = None
+
+                    if 'geometry' in result:
+                        lat = result['geometry']['location']['lat']
+                        lng = result['geometry']['location']['lng']
+
+                    if 'place_id' in result:
+                        place_id = result['place_id']
+
+                    if 'photos' in result:
+                        photo_name = self.get_photo(result['photos'][0]['photo_reference'])
+
+                    data.append({
+                        "name": name,
+                        "open_now":open_now,
+                        "formatted_address":formatted_address,
+                        "rating":rating,
+                        "loclat":lat,
+                        "loclng":lng,
+                        "place_id":place_id,
+                        "photo_name":photo_name
+                    })
+                return data
+            elif option == 'city':
+                for result in results:
+                    if result['name']:
+                        name = result['name']
+
+                    if 'formatted_address' in result:
+                        formatted_address = result['formatted_address']
+
+                    if 'geometry' in result:
+                        lat = result['geometry']['location']['lat']
+                        lng = result['geometry']['location']['lng']
+
+                    if 'place_id' in result:
+                        place_id = result['place_id']
+
+                    if 'photos' in result:
+                        photo_name = self.get_photo(result['photos'][0]['photo_reference'])
+                    data.append({
+                        "name": name,
+                        "formatted_address":formatted_address,
+                        "loclat":lat,
+                        "loclng":lng,
+                        "place_id":place_id,
+                        "photo_name":photo_name
+                    })
+                return data
+        except Exception as e:
+            print(e)
+            return []

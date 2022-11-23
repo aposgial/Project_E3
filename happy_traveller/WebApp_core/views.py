bb3266f8ea2d86b8f9b37d044147c65322d8e809
aposgial <apostolosgls@gmail.com>
2022-11-23 18:22:49 +0200

    webapp_core view desplay row data to home html file

--- happy_traveller/WebApp_core/views.py
+++ happy_traveller/WebApp_core/views.py
@@ -4,11 +4,10 @@ from google_APIs.controller import API_Controller
 # Create your views here.
 def home(request):
     api = API_Controller()
+
     tourist_attraction = api.get_random_country_places(type='tourist_attraction')[:5]
-    museum = api.get_random_country_places(type='museum')
-    park = api.get_random_country_places(type='park')
-
-
+    museum = api.get_random_country_places(type='museum')[:5]
+    park = api.get_random_country_places(type='park')[:5]
 
     if request.method == 'GET':
         search = request.GET.get('search')
@@ -35,5 +34,9 @@ def home(request):
                 "tourist_attraction":tourist_attraction,
                 "museum":museum,
                 "park":park}
-        
+
+    context['tourist_attraction'] = tourist_attraction,
+    context['museum']
+    context['park']
+
     return render(request, 'WebApp_core/home.html', context=context)

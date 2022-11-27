4ecc5d9d1ce7aad5fc46fc97d0f229e40fa6cfcf
aposgial <apostolosgls@gmail.com>
2022-11-27 15:45:41 +0200

    method place_details in WebApp_core/views, url routing and place_details html created

--- happy_traveller/WebApp_core/views.py
+++ happy_traveller/WebApp_core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from google_APIs.controller import API_Controller
 from happy_traveller.mixins import get_random_country
 
@@ -6,8 +7,19 @@ from happy_traveller.mixins import get_random_country
 def home(request):
     api = API_Controller()
     country = get_random_country()
-    samples = 3
+    samples = 2
     context = {}
+
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        #option = request.GET.get('flexRadioDefault')
+        print('ok1')
+        if search:
+            result = api.get_place_by_search(search_input=search)
+            #result['photos_names'] = api.get_photos_from_place(photos=result['photos'][:samples])
+            
+            print('ok2')
+            return render(request, 'WebApp_core/place_details.html', context={"result":result})
 
     places = api.get_places(type='tourist_attraction', country=country)[:samples]
     tourist_attraction = api.get_photo_from_all_places(places)
@@ -18,36 +30,6 @@ def home(request):
     places = api.get_places(type='park', country=country)[:samples]
     park = api.get_photo_from_all_places(places)
 
-    if request.method == 'GET':
-        search = request.GET.get('search')
-        option = request.GET.get('flexRadioDefault')
-
-        if search and option:
-            api.search_location = search
-            infos = api.get_places_info()
-            results = []
-            
-            for info in infos:
-                if info['category'] == 'city' and option == 'city':
-                    results.append(info)
-                    
-
-                elif info['category'] == 'place' and option == 'place':
-                    results.append(info)
-                    
-            context = {
-                "search":True,
-                "results":results}
-        else:
-            context = {
-                "search":False}
-    
-    if len(tourist_attraction) % 2 == 0:
-        #temp = zip(*(iter(tourist_attraction),)*group)
-        pass
-    else:
-        pass
-
     context = {
         "country": country,
         "tourist_attraction": tourist_attraction,
@@ -56,5 +38,8 @@ def home(request):
     }
 
     #zip(*(iter(park),)*group)
-    
     return render(request, 'WebApp_core/home.html', context=context)
+
+
+def place_details(request):
+    return render(request, 'WebApp_core/place_details.html')
